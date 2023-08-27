ee177d06d381f4bb219441e0a92a8cb7bc62f7a7
realNoddy <32793505+realNoddy@users.noreply.github.com>
2023-08-27 13:23:38 +0200

    Theme template upload

---
+++ ShopFeedback/src/ShopFeedback.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ShopFeedback;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ShopFeedback extends Plugin implements ThemeInterface
+{
+}
