e5fd360a363b81f513581deb2625fe1a07abf044
realNoddy <32793505+realNoddy@users.noreply.github.com>
2023-08-29 01:23:43 +0200

    override AddToCart class with own
    trigger on moving product to cart
    disables the default offcanvascart popup from the side
    updating the cart

--- ShopFeedback/src/Resources/app/storefront/src/main.js
+++ ShopFeedback/src/Resources/app/storefront/src/main.js
@@ -1,1 +1,6 @@
-console.log("Why no work?");
+console.log("Why work?");
+
+import Feedback from "./script/feedback";
+
+const PluginManager = window.PluginManager;
+PluginManager.override('AddToCart', Feedback, '[data-add-to-cart]');
--- ShopFeedback/src/Resources/app/storefront/src/script/feedback.js
+++ ShopFeedback/src/Resources/app/storefront/src/script/feedback.js
@@ -1,7 +1,42 @@
-console.log("feedback");
+
 //OffCanvasCart
 //AddToCart
 
-
-PluginManager.getPlugin('AddToCart').$emitter.subscribe('openOffCanvasCart', ()=>{console.log("");});
+//PluginManager.getPlugin('AddToCart').$emitter.subscribe('openOffCanvasCart', ()=>{console.log("");});
 //https://developer.shopware.com/docs/guides/plugins/plugins/storefront/reacting-to-javascript-events#registering-to-events
+
+import AddToCartPlugin from 'src/plugin/add-to-cart/add-to-cart.plugin';
+import DomAccess from 'src/helper/dom-access.helper';
+import HttpClient from 'src/service/http-client.service';
+import FormSerializeUtil from 'src/utility/form/form-serialize.util';
+
+
+export default class Feedback extends AddToCartPlugin {
+
+    init() {
+        console.log("feedback init");
+        this._cart = DomAccess.querySelector(document, '.header-cart');
+        this._client = new HttpClient(window.accessKey, window.contextToken);
+        super.init();
+    }
+
+    _openOffCanvasCart(instance, requestUrl, formData) {
+        this._client.post(requestUrl, formData, this._afterAddItemToCart());
+    }
+
+    _afterAddItemToCart(){
+        this._refreshCart();
+        const formData = FormSerializeUtil.serialize(this._form);
+        console.log(FormSerializeUtil.serializeJson(this._form));
+    }
+
+    _refreshCart(){
+        const cartWidget = DomAccess.querySelector(this._cart, '[data-cart-widget]');
+        const cartWidgetInstance = window.PluginManager.getPluginInstanceFromElement(cartWidget, 'CartWidget');
+        setTimeout(()=>{
+            cartWidgetInstance.fetch();
+        },50);
+        
+    }
+
+}
