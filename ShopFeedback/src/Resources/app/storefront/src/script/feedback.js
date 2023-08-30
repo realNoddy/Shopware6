c9dc8fbb4897f6b807e3fdfef3b739c50ef1a6b3
realNoddy <32793505+realNoddy@users.noreply.github.com>
2023-08-30 03:27:07 +0200

    feedback added

--- ShopFeedback/src/Resources/app/storefront/src/script/feedback.js
+++ ShopFeedback/src/Resources/app/storefront/src/script/feedback.js
@@ -1,10 +1,3 @@
-
-//OffCanvasCart
-//AddToCart
-
-//PluginManager.getPlugin('AddToCart').$emitter.subscribe('openOffCanvasCart', ()=>{console.log("");});
-//https://developer.shopware.com/docs/guides/plugins/plugins/storefront/reacting-to-javascript-events#registering-to-events
-
 import AddToCartPlugin from 'src/plugin/add-to-cart/add-to-cart.plugin';
 import DomAccess from 'src/helper/dom-access.helper';
 import HttpClient from 'src/service/http-client.service';
@@ -14,9 +7,17 @@ import FormSerializeUtil from 'src/utility/form/form-serialize.util';
 export default class Feedback extends AddToCartPlugin {
 
     init() {
-        console.log("feedback init");
-        this._cart = DomAccess.querySelector(document, '.header-cart');
+        
+        this._cart = DomAccess.querySelector(document, '.header-cart [data-cart-widget]');
         this._client = new HttpClient(window.accessKey, window.contextToken);
+
+        if (typeof Feedback.notification === 'undefined') {
+            const notificationDiv = document.createElement('div');
+            notificationDiv.id ='shop-feedback-notification';
+            document.querySelector('body').append(notificationDiv);
+            Feedback.notification = notificationDiv;
+        }
+
         super.init();
     }
 
@@ -25,18 +26,56 @@ export default class Feedback extends AddToCartPlugin {
     }
 
     _afterAddItemToCart(){
+        const formButton = DomAccess.querySelector(this._form, '.btn-buy');
+        formButton.classList.add('shop-feedback');
+        formButton.innerText = formButton.dataset.addToCartText;
+        formButton.disabled = true;
+        setTimeout(()=>{
+            formButton.classList.remove('shop-feedback');
+            formButton.innerText = formButton.title;
+            formButton.disabled = false;
+        },1000);
+        this._showNotification(this._form);
         this._refreshCart();
-        const formData = FormSerializeUtil.serialize(this._form);
-        console.log(FormSerializeUtil.serializeJson(this._form));
+    }
+
+    _showNotification(productForm){
+        const productNotification = document.createElement('div');
+        productNotification.classList.add('shop-feedback-item-notification');
+        productNotification.classList.add('notification-show');
+        const productData = FormSerializeUtil.serializeJson(productForm);
+        let productPrice = "0";
+        if (productForm.id == "productDetailPageBuyProductForm"){
+            productPrice = productForm.closest('.product-detail-content').querySelector('.product-detail-price').innerText;
+        }else{
+            productPrice = productForm.closest('.product-info').querySelector('.product-price').innerText;
+        }
+        
+        let productQuantity = 1;
+        for (const key in productData) {
+            if (key.includes('[quantity]')) {
+                productQuantity = productData[key];
+                break;
+            }
+        }
+        productNotification.innerHTML = "<h5>"+productData['product-name']+"</h5>"+productQuantity+"x <strong>"+productPrice+"</strong>";
+        Feedback.notification.append(productNotification);
+        productNotification.addEventListener('click', ()=>{
+            productNotification.classList.remove('notification-show');
+            productNotification.classList.add('notification-remove');
+        });
+        productNotification.addEventListener("animationend", () => {
+            productNotification.remove();
+        });
     }
 
     _refreshCart(){
-        const cartWidget = DomAccess.querySelector(this._cart, '[data-cart-widget]');
-        const cartWidgetInstance = window.PluginManager.getPluginInstanceFromElement(cartWidget, 'CartWidget');
+        const cartWidgetInstance = window.PluginManager.getPluginInstanceFromElement(this._cart, 'CartWidget');
+        this._cart.classList.add('cart-feedback');
         setTimeout(()=>{
             cartWidgetInstance.fetch();
         },50);
-        
     }
 
 }
+
